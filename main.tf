54e289a088b9ddca2f3dba886fe81e84057a0e91
tanyaa09 <iamtanya940@gmail.com>
2026-07-02 17:53:10 +0530

    k8

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  kubernetes_version  = "1.30.0"
+
+  default_node_pool {
+    name                = "systempool"
+    node_count          = var.node_count
+    vm_size             = var.vm_size
+    os_disk_size_gb     = 30
+    type                = "VirtualMachineScaleSets"
+    auto_scaling_enabled = true
+    min_count           = 1
+    max_count           = 3
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+resource_group_name = "aks-rg"
+location            = "Central India"
+cluster_name        = "tanya-aks-cluster"
+dns_prefix          = "tanyaaks"
+node_count          = 2
+vm_size             = "Standard_DS2_v2"
+environment         = "dev"
